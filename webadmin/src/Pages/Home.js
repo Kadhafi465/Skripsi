cf29b73f5ec4055795b87ef0f84851aa3730e3bc
Indra Puji Novirwan <indrapuji@gmail.com>
2020-08-07 22:43:59 +0700

    remove home

--- webadmin/src/Pages/Home.js
+++ webadmin/src/Pages/Home.js
@@ -6,69 +6,6 @@ export default () => {
   return (
     <>
       <Navigation />
-
-      <div
-        style={{
-          width: 250,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: "lightblue",
-          marginTop: 50,
-          marginLeft: 580,
-        }}
-      >
-        <h1 style={{ fontSize: 20, textAlign: "center", textJustify: "center" }}>Kelola User</h1>
-      </div>
-      <div
-        style={{
-          width: 250,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: "lightblue",
-          marginTop: 20,
-          marginLeft: 580,
-        }}
-      >
-        <h1 style={{ fontSize: 20, textAlign: "center", textJustify: "center" }}>Kelola Jadwal</h1>
-      </div>
-      <div
-        style={{
-          width: 250,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: "lightblue",
-          marginTop: 20,
-          marginLeft: 580,
-        }}
-      >
-        <h1 style={{ fontSize: 20, textAlign: "center", textJustify: "center" }}>
-          Kelola Pembayaran
-        </h1>
-      </div>
-      <div
-        style={{
-          width: 250,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: "lightblue",
-          marginTop: 20,
-          marginLeft: 580,
-        }}
-      >
-        <h1 style={{ fontSize: 20, textAlign: "center", textJustify: "center" }}>Laporan</h1>
-      </div>
-      <div
-        style={{
-          width: 250,
-          height: 50,
-          borderRadius: 10,
-          backgroundColor: "lightgreen",
-          marginTop: 20,
-          marginLeft: 580,
-        }}
-      >
-        <h1 style={{ fontSize: 20, textAlign: "center", textJustify: "center" }}>Logout</h1>
-      </div>
     </>
   );
 };
